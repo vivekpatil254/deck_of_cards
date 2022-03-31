894b723c8beeef05b14ccbe484533baacb9eb6c9
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 <Vivekpatil254@gmail.com>
2022-03-31 15:43:56 +0530

    Distribution of cards and suit count done

--- src/com/bridgelabz/DeckOfCards.java
+++ src/com/bridgelabz/DeckOfCards.java
@@ -44,7 +44,7 @@ public class DeckOfCards {
         int i,j;
         if (Players.numberOfPlayers == 2) {
             System.out.println("\n\tPlayer1 \t\t\t\t Player 2");
-            for(i=0,j=0;i<18;i++,j++)
+            for(i=0,j=0;i<9;i++,j++)
             {
                 suit1[i]=suit[j];
                 rank1[i]=rank[j];
@@ -52,33 +52,33 @@ public class DeckOfCards {
                 suit2[i]=suit[j];
                 rank2[i]=rank[j];
                 System.out.println("\t"+rank1[i]+"  "+suit1[i]+"\t\t\t"+rank2[i]+"  "+suit2[i]);
-                if(suit1[i]=="club")
-                    player1SuitCount[1]++;
+                if(suit1[i]=="clubs")
+                    player1SuitCount[0]++;
                 if(suit1[i]=="diamonds")
-                    player1SuitCount[2]++;
+                    player1SuitCount[1]++;
                 if(suit1[i]=="hearts")
-                    player1SuitCount[3]++;
-                if(suit1[i]=="spades")
-                    player1SuitCount[4]++;
-
-                if(suit2[i]=="club")
-                    player2SuitCount[1]++;
-                if(suit2[i]=="diamonds")
                     player1SuitCount[2]++;
-                if(suit2[i]=="hearts")
+                if(suit1[i]=="spades")
                     player1SuitCount[3]++;
+
+                if(suit2[i]=="clubs")
+                    player2SuitCount[0]++;
+                if(suit2[i]=="diamonds")
+                    player2SuitCount[1]++;
+                if(suit2[i]=="hearts")
+                    player2SuitCount[2]++;
                 if(suit2[i]=="spades")
-                    player1SuitCount[4]++;
+                    player2SuitCount[3]++;
                 }
-            System.out.println("\t Clubs  "+player1SuitCount[1]+"\t\t\t\t"+" Clubs  "+player2SuitCount[1]);
-            System.out.println("\t Diamonds "+player1SuitCount[2]+"\t\t\t"+" Diamomds "+player2SuitCount[2]);
-            System.out.println("\t Hearts "+player1SuitCount[3]+"\t\t\t"+" Hearts "+player2SuitCount[3]);
-            System.out.println("\t Spades "+player1SuitCount[4]+"\t\t\t"+" Spades "+player2SuitCount[4]);
+            System.out.println("\t clubs  "+player1SuitCount[0]+"\t\t\t"+" clubs  "+player2SuitCount[0]);
+            System.out.println("\t diamonds "+player1SuitCount[1]+"\t\t\t"+" diamomds "+player2SuitCount[1]);
+            System.out.println("\t hearts "+player1SuitCount[2]+"\t\t\t"+" hearts "+player2SuitCount[2]);
+            System.out.println("\t spades "+player1SuitCount[3]+"\t\t\t"+" spades "+player2SuitCount[3]);
         }
 
         if (Players.numberOfPlayers == 3) {
             System.out.println("\n\tPlayer1 \t\t\t\t Player 2 \t\t\t\t Player 3");
-            for(i=0,j=0;i<27;i++,j++)
+            for(i=0,j=0;i<9;i++,j++)
             {
                 suit1[i]=suit[j];
                 rank1[i]=rank[j];
@@ -90,43 +90,43 @@ public class DeckOfCards {
                 rank3[i]=rank[j];
                 System.out.println("\t"+rank1[i]+"  "+suit1[i]+"\t\t\t\t"+rank2[i]+"  "+suit2[i]+"\t\t\t\t"+rank3[i]+"  "+suit3[i]);
 
-                if(suit1[i]=="club")
-                    player1SuitCount[1]++;
+                if(suit1[i]=="clubs")
+                    player1SuitCount[0]++;
                 if(suit1[i]=="diamonds")
-                    player1SuitCount[2]++;
+                    player1SuitCount[1]++;
                 if(suit1[i]=="hearts")
-                    player1SuitCount[3]++;
+                    player1SuitCount[2]++;
                 if(suit1[i]=="spades")
-                    player1SuitCount[4]++;
+                    player1SuitCount[3]++;
 
-                if(suit2[i]=="club")
-                    player2SuitCount[1]++;
+                if(suit2[i]=="clubs")
+                    player2SuitCount[0]++;
                 if(suit2[i]=="diamonds")
-                    player2SuitCount[2]++;
+                    player2SuitCount[1]++;
                 if(suit2[i]=="hearts")
-                    player2SuitCount[3]++;
+                    player2SuitCount[2]++;
                 if(suit2[i]=="spades")
-                    player2SuitCount[4]++;
+                    player2SuitCount[3]++;
 
-                if(suit3[i]=="club")
-                    player3SuitCount[1]++;
+                if(suit3[i]=="clubs")
+                    player3SuitCount[0]++;
                 if(suit3[i]=="diamonds")
-                    player3SuitCount[2]++;
+                    player3SuitCount[1]++;
                 if(suit3[i]=="hearts")
-                    player3SuitCount[3]++;
+                    player3SuitCount[2]++;
                 if(suit3[i]=="spades")
-                    player3SuitCount[4]++;
+                    player3SuitCount[3]++;
             }
-           System.out.println("\t Clubs  "+player1SuitCount[1]+"\t\t\t\t"+" Clubs  "+player2SuitCount[1]+"\t\t\t\t"+" Clubs  "+player3SuitCount[1]);
-            System.out.println("\t Diamonds "+player1SuitCount[2]+"\t\t\t"+" Diamomds "+player2SuitCount[2]+"\t\t\t"+" Diamomds "+player3SuitCount[2]);
-            System.out.println("\t Hearts "+player1SuitCount[3]+"\t\t\t"+" Hearts "+player2SuitCount[3]+"\t\t\t"+" Hearts "+player3SuitCount[3]);
-            System.out.println("\t Spades "+player1SuitCount[4]+"\t\t\t"+" Spades "+player2SuitCount[4]+"\t\t\t"+" Spades "+player4SuitCount[4]);
+           System.out.println("\t Clubs  "+player1SuitCount[0]+"\t\t\t"+" Clubs  "+player2SuitCount[0]+"\t\t\t\t"+" Clubs  "+player3SuitCount[0]);
+            System.out.println("\t Diamonds "+player1SuitCount[1]+"\t\t\t"+" Diamomds "+player2SuitCount[1]+"\t\t\t"+" Diamomds "+player3SuitCount[1]);
+            System.out.println("\t Hearts "+player1SuitCount[2]+"\t\t\t"+" Hearts "+player2SuitCount[2]+"\t\t\t"+" Hearts "+player3SuitCount[2]);
+            System.out.println("\t Spades "+player1SuitCount[3]+"\t\t\t"+" Spades "+player2SuitCount[3]+"\t\t\t"+" Spades "+player3SuitCount[3]);
 
         }
 
         if (Players.numberOfPlayers == 4) {
             System.out.println("\n\tPlayer1 \t\t\t\t Player 2 \t\t\t\t Player 3\t\t\t\t Player 4");
-            for(i=0,j=0;i<36;i++,j++)
+            for(i=0,j=0;i<9;i++,j++)
             {
                 suit1[i]=suit[j];
                 rank1[i]=rank[j];
@@ -142,47 +142,47 @@ public class DeckOfCards {
                 System.out.println("\t"+rank1[i]+"  "+suit1[i]+"\t\t\t"+rank2[i]+"  "+suit2[i]+"\t\t\t"+rank3[i]+"  "+suit3[i]+"\t\t\t"+rank4[i]+"  "+suit4[i]);
 
 
-                if(suit1[i]=="club")
-                    player1SuitCount[1]++;
+                if(suit1[i]=="clubs")
+                    player1SuitCount[0]++;
                 if(suit1[i]=="diamonds")
-                    player1SuitCount[2]++;
+                    player1SuitCount[1]++;
                 if(suit1[i]=="hearts")
-                    player1SuitCount[3]++;
+                    player1SuitCount[2]++;
                 if(suit1[i]=="spades")
-                    player1SuitCount[4]++;
+                    player1SuitCount[3]++;
 
-                if(suit2[i]=="club")
-                    player2SuitCount[1]++;
+                if(suit2[i]=="clubs")
+                    player2SuitCount[0]++;
                 if(suit2[i]=="diamonds")
-                    player2SuitCount[2]++;
+                    player2SuitCount[1]++;
                 if(suit2[i]=="hearts")
-                    player2SuitCount[3]++;
+                    player2SuitCount[2]++;
                 if(suit2[i]=="spades")
-                    player2SuitCount[4]++;
+                    player2SuitCount[3]++;
 
-                if(suit3[i]=="club")
-                    player3SuitCount[1]++;
+                if(suit3[i]=="clubs")
+                    player3SuitCount[0]++;
                 if(suit3[i]=="diamonds")
-                    player3SuitCount[2]++;
+                    player3SuitCount[1]++;
                 if(suit3[i]=="hearts")
-                    player3SuitCount[3]++;
+                    player3SuitCount[2]++;
                 if(suit3[i]=="spades")
-                    player3SuitCount[4]++;
+                    player3SuitCount[3]++;
 
-                if(suit4[i]=="club")
-                    player4SuitCount[1]++;
+                if(suit4[i]=="clubs")
+                    player4SuitCount[0]++;
                 if(suit4[i]=="diamonds")
-                    player4SuitCount[2]++;
+                    player4SuitCount[1]++;
                 if(suit4[i]=="hearts")
-                    player4SuitCount[3]++;
+                    player4SuitCount[2]++;
                 if(suit4[i]=="spades")
-                    player4SuitCount[4]++;
+                    player4SuitCount[3]++;
             }
 
-            System.out.println("\t Clubs  "+player1SuitCount[1]+"\t\t\t\t"+" Clubs  "+player2SuitCount[1]+"\t\t\t\t"+" Clubs  "+player3SuitCount[1]+"\t\t\t\t"+" Clubs  "+player4SuitCount[1]);
-            System.out.println("\t Diamonds "+player1SuitCount[2]+"\t\t\t"+" Diamomds "+player2SuitCount[2]+"\t\t\t"+" Diamomds "+player3SuitCount[2]+"\t\t\t"+" Diamomds "+player4SuitCount[2]);
-            System.out.println("\t Hearts "+player1SuitCount[3]+"\t\t\t"+" Hearts "+player2SuitCount[3]+"\t\t\t"+" Hearts "+player3SuitCount[3]+"\t\t\t"+" Hearts "+player4SuitCount[3]);
-            System.out.println("\t Spades "+player1SuitCount[4]+"\t\t\t"+" Spades "+player2SuitCount[4]+"\t\t\t"+" Spades "+player3SuitCount[4]+"\t\t\t"+" Spades "+player4SuitCount[4]);
+            System.out.println("\t Clubs  "+player1SuitCount[0]+"\t\t\t"+" Clubs  "+player2SuitCount[0]+"\t\t\t"+" Clubs  "+player3SuitCount[0]+"\t\t\t"+" Clubs  "+player4SuitCount[0]);
+            System.out.println("\t Diamonds "+player1SuitCount[1]+"\t\t\t"+" Diamomds "+player2SuitCount[1]+"\t\t\t"+" Diamomds "+player3SuitCount[1]+"\t\t\t"+" Diamomds "+player4SuitCount[1]);
+            System.out.println("\t Hearts "+player1SuitCount[2]+"\t\t\t"+" Hearts "+player2SuitCount[2]+"\t\t\t"+" Hearts "+player3SuitCount[2]+"\t\t\t"+" Hearts "+player4SuitCount[2]);
+            System.out.println("\t Spades "+player1SuitCount[3]+"\t\t\t"+" Spades "+player2SuitCount[3]+"\t\t\t"+" Spades "+player3SuitCount[3]+"\t\t\t"+" Spades "+player4SuitCount[3]);
 
         }
 
